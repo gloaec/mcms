2c6e226b66d6ee74f2cadb6dcf334f50c3ab1171
Frédéric Péters <fpeters@entrouvert.com>
2015-08-21 12:19:04 +0200

    misc: spelling fix in "no update available"

--- www/js/index.js
+++ www/js/index.js
@@ -245,7 +245,7 @@ var app = {
                         app.flash(tmpl('momo-update-available-tmpl', { mtime: app.utils.formatDate(mtime) }), 'success');
                         app.utils.setLoadingMsg("Mise à jour disponible !");
                     } else {
-                        app.utils.setLoadingMsg("Aucune nouvelles mises à jour");
+                        app.utils.setLoadingMsg("Aucune nouvelle mises à jour");
                     }
                     if(typeof resolve === 'function'){
                         resolve(updateAvailable);
