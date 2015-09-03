110eff15f054dcfa64c50d7d02c01b54168b7a11
Frédéric Péters <fpeters@entrouvert.com>
2015-09-03 17:00:25 +0200

    turn assets/index.js into a silent example script (#8162)

--- www/assets/index.js
+++ www/assets/index.js
@@ -1,3 +1,7 @@
-setTimeout(function(){
-    app.flash('<i class="fa fa-arrow-up pull-right" style="margin-right: 10px;"></i> There is a search tool available here', 'info');
-}, 10000);
+/* this script will be run after the assets are loaded */
+/* it could for example flash some new information, like this:
+ * setTimeout(function(){
+ *    app.flash('<i class="fa fa-arrow-up pull-right" style="margin-right: 10px;"></i>' +
+ *              'There is a search tool available here', 'info');
+ * }, 10000);
+ */
